ad9dd644ac295af0c7ea7efd7c7249a35561f161
lc <zhuyuanliucheng@163.com>
2018-07-08 11:06:15 +0800

    从数据库中读取shiro的权限配置

---
+++ src/main/java/com/lzapi/dao/ShiroFilterDao.java
@@ -0,0 +1,12 @@
+package com.lzapi.dao;
+
+import com.lzapi.pojo.ShiroFilter;
+
+import java.util.List;
+
+/**
+ * Created by lc on 2018/7/2.
+ */
+public interface ShiroFilterDao {
+    List<ShiroFilter> getShiroFilter();
+}
