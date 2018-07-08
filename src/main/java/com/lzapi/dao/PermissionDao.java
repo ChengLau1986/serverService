3f0f58eccf22e00d9b002ad98f0467850e727b4a
lc <zhuyuanliucheng@163.com>
2018-07-08 11:09:43 +0800

    从数据库中读取权限配置

--- src/main/java/com/lzapi/dao/PermissionDao.java
+++ src/main/java/com/lzapi/dao/PermissionDao.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * Created by lc on 2018/6/7.
+ * Created by lc on 2018/6/71.
  */
 public interface PermissionDao {
     User getUserPermission(@Param("username") String username);
